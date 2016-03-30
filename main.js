a386d309153c6d35173dc45b81c35a0dcd9820d5
mittelmabigkeit <comp422012016@mail.ru>
2016-03-30 16:35:17 +0300

    aa

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 (function() {
 	var div=document.getElementById('test'),
 	    style=div.style;
-	    div.className=" css-class  css-class-new ";
-	    div.className=div.className.replace("css-class-new", "");
+	   // div.className=" css-class  css-class-new ";
+	    //div.className=div.className.replace("css-class-new", "");
+	    div.classList.add('css-class');
+	    div.classList.remove ('css-class');
+	    div.classList.toggle('css-class');
 })();
